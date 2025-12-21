2ae0e0e5ea5c216dd6957d7dbe762f81a3c3a123
type/arc/anything you like <ashfaquemaroof8@gmail.com>
2025-12-21 13:17:04 +0000

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-12-21 12:22:20
+set PATH $PATH /home/typeaww/.local/bin
+
+pokemon-colorscripts -r | tail -n +2 
